6f16cbfc4bd741e672f1523e5cfe69ff59bb628b
selma <belhadjselma1@gmail.com>
2022-09-11 23:41:37 +0100

    Import Components in the App" "

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import About from './components/about/About';
+import Experience from './components/experience/Experience';
+import Services from './components/services/Services';
+import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
