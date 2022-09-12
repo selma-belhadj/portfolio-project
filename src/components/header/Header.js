4cabbabdca96ecf4514c1e1bcf86fa79060fa600
selma <belhadjselma1@gmail.com>
2022-09-12 12:27:32 +0100

    Add the header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import CTA from './CTA';
 import './header.css';
+import ME from '../../assets/me.png';
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => (
-  <div>Header</div>
+  <header>
+    <div className="container header__container">
+      <h5>Hello I&apos;m</h5>
+      <h1>Selma Belhadj</h1>
+      <h5 className="text-light">Fullstack developer</h5>
+      <CTA />
+      <HeaderSocials />
+
+      <div className="me">
+        <img src={ME} alt="me-pic" />
+      </div>
+      <a href="#contact" className="scroll__down">Scroll Down</a>
+
+    </div>
+  </header>
 );
 
 export default Header;
