c3adc1619e7bc589419aed0924edef53c0de83c4
selma <belhadjselma1@gmail.com>
2022-09-13 12:27:07 +0100

    Add some items to the About component

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,7 +2,18 @@ import React from 'react';
 import './about.css';
 
 const About = () => (
-  <section id="about">About</section>
+  <section id="about">
+    <h5>Get To Know</h5>
+    <h2>About Me</h2>
+    <div className="container about__container">
+      <div className="about_me">
+        <div className="about__me-image">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+
+  </section>
 );
 
 export default About;
