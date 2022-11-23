56f11cbdc6ed18ff677ba262c4b9d43ccda75845
selma <belhadjselma1@gmail.com>
2022-11-23 21:32:19 +0100

    Updated the header section js & css content

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import CTA from './CTA';
 import './header.css';
-import ME from '../../assets/me.png';
+import ME from '../../assets/me1.png';
 import HeaderSocials from './HeaderSocials';
 
 const Header = () => (
