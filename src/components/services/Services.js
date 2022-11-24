de2ffb732b50fd7c1e52836dc1c7d51e84369a2d
selma <belhadjselma1@gmail.com>
2022-11-24 09:07:44 +0100

    Added the services component

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,8 +1,112 @@
 import React from 'react';
 import './services.css';
+import { BiCheck } from 'react-icons/bi';
 
 const Services = () => (
-  <section id="services">Services</section>
+  <section id="services">
+    <h5>What I Offer</h5>
+    <h2>Services</h2>
+    <div className="container services__container">
+      <article className="service">
+        <div className="head__service">
+          <h3>UI/UX design</h3>
+        </div>
+        <ul className="service__list">
+          <li>
+            <BiCheck className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+        </ul>
+      </article>
+      {/* End of UI/UX Design */}
+
+      <article className="service">
+        <div className="head__service">
+          <h3>Web develepement</h3>
+        </div>
+        <ul className="service__list">
+          <li>
+            <BiCheck className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+        </ul>
+      </article>
+      {/* End of Web develepement */}
+
+      <article className="service">
+        <div className="head__service">
+          <h3>Content Creation</h3>
+        </div>
+        <ul className="service__list">
+          <li>
+            <BiCheck className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+        </ul>
+      </article>
+      {/* End of Content Creation */}
+    </div>
+  </section>
 );
 
 export default Services;
