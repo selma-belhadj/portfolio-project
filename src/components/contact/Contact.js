40018f1b18faaea3d3c78cc9ac8a74560bfe9d78
selma <belhadjselma1@gmail.com>
2022-11-25 11:00:51 +0100

    Styled the contact component

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -7,24 +7,29 @@ const Contact = () => (
   <section id="contact">
     <h5>Get In Touch</h5>
     <h2>Contact Me</h2>
-    <div className="container contact_container">
-      <div className="contact options">
+    <div className="container contact__container">
+      <div className="contact__options">
         <article className="contact__option">
-          <MdOutlineMailOutline />
+          <MdOutlineMailOutline className="contact__option-icon" />
           <h4>Email</h4>
           <h5>belhadjselma1@gmail.com</h5>
           <a href="mailto:belhadjselma1@gmail.com" target="_blank" rel="noreferrer">Send a message</a>
         </article>
 
         <article className="contact__option">
-          <RiTwitterLine />
+          <RiTwitterLine className="contact__option-icon" />
           <h4>Twitter</h4>
           <h5>Selma Belhadj</h5>
           <a href="https://twitter.com/selma_bel_hadj" target="_blank" rel="noreferrer">Twitter a message</a>
         </article>
       </div>
       {/* End of contact options */}
-      
+      <form action="">
+        <input type="text" name="name" placeholder="Your fullname" required />
+        <input type="email" name="Email" placeholder="Your email" required />
+        <textarea name="message" id="" rows="7" placeholder="Your message" required />
+        <button type="submit" className="btn btn-primary">Send Message</button>
+      </form>
     </div>
   </section>
 );
